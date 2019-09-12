9620880a4734a6771e0019ef6abab6b224ed9483
bgarf <b.garfin@hotmail.co.uk>
2019-09-12 14:53:15 +0100

    add multiple spring profiles

--- src/test/java/com/projects/films/controller/FilmControllerTest.java
+++ src/test/java/com/projects/films/controller/FilmControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
+@ActiveProfiles("test")
 public class FilmControllerTest {
 
     @MockBean
--- src/test/java/com/projects/films/data/FilmRepositoryTest.java
+++ src/test/java/com/projects/films/data/FilmRepositoryTest.java
@@ -6,12 +6,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @DataMongoTest
+@ActiveProfiles("test")
 public class FilmRepositoryTest {
 
     @Autowired
